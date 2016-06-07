c8f23d1c1297039e52e511b1b5b88ac6eb0320c3
Katherine De La Cruz <kdelacruz@rcp.pe>
2016-06-07 18:06:50 -0500

    Add sidenav collapse

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ function toggleActive () {
   $('.menu-item').on('click', function() {
     $(this).toggleClass('active');
   });
+
+  $('.bar-menu').on('click', function() {
+    $(this).toggleClass('active');
+    $('.sidenav').toggleClass('collapse');
+    $('.view-content').toggleClass('collapse');
+    $('.toolbar').toggleClass('collapse');
+  });
 }
