b70b46ac5b453c90d188067c4bb6c432145e9a8a
Katherine De La Cruz <kdelacruz@rcp.pe>
2016-06-17 17:39:38 -0500

    Add login view

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   toggleActive();
+  button_validate();
+  $('#username, #password').keydown(button_validate);
+  $('.color').click( function () {
+    $(this).addClass('click');
+  });
 });
 
 function toggleActive () {
@@ -27,5 +32,15 @@ function toggleActive () {
     }
   });
 
+};
 
+function button_validate() {
+  if($('#username').val().length > 0 && $('#password').val().length > 6) {
+    $('.button-ln').addClass('wrapper');
+    $('#validate-link').removeClass('not-active');
+  }
+  else {
+    $('.button-ln').removeClass('wrapper');
+    $('#validate-link').addClass('not-active');
+  }
 }
